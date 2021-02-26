eb52e715b82714d76e1f1dd10f29ae60c161b409
Victor Rodriguez <victor.i.rodriguez@oracle.com>
2021-02-26 13:56:30 -0600

    Cookbook session

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -8,11 +8,91 @@
 /*
  * Your dashboard ViewModel code goes here
  */
-define(['accUtils'],
- function(accUtils) {
+define(['knockout','accUtils', 'ojs/ojbootstrap', 'ojs/ojarraydataprovider', 'ojs/ojchart', 'ojs/ojselectsingle'],
+ function(ko, accUtils, Bootstrap, ArrayDataProvider) {
     function DashboardViewModel() {
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
+
+      this.val = ko.observable('pie');
+  
+      var browsers = [
+        { value: 'pie', label: 'Pie' },
+        { value: 'line', label: 'Line' },
+        { value: 'bar', label: 'Bar' },
+
+      ];
+  
+      this.browsersDP = new ArrayDataProvider(browsers, { keyAttributes: 'value' });
+
+      //
+
+      let data =   [
+        {
+          "id": 0,
+          "series": "Series 1",
+          "group": "Group A",
+          "value": 42
+        },
+        {
+          "id": 1,
+          "series": "Series 1",
+          "group": "Group B",
+          "value": 34
+        },
+        {
+          "id": 2,
+          "series": "Series 2",
+          "group": "Group A",
+          "value": 55
+        },
+        {
+          "id": 3,
+          "series": "Series 2",
+          "group": "Group B",
+          "value": 30
+        },
+        {
+          "id": 4,
+          "series": "Series 3",
+          "group": "Group A",
+          "value": 36
+        },
+        {
+          "id": 5,
+          "series": "Series 3",
+          "group": "Group B",
+          "value": 50
+        },
+        {
+          "id": 6,
+          "series": "Series 4",
+          "group": "Group A",
+          "value": 22
+        },
+        {
+          "id": 7,
+          "series": "Series 4",
+          "group": "Group B",
+          "value": 46
+        },
+        {
+          "id": 8,
+          "series": "Series 5",
+          "group": "Group A",
+          "value": 22
+        },
+        {
+          "id": 9,
+          "series": "Series 5",
+          "group": "Group B",
+          "value": 46
+        }
+      ];
+
+      this.stackValue = ko.observable('off');
+      this.orientationValue = ko.observable('vertical');
+      this.dataProvider = new ArrayDataProvider(data, {keyAttributes: 'id'});
 
       /**
        * Optional ViewModel method invoked after the View is inserted into the
